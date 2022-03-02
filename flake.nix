384073f8823f88e07db2c7017b2938f14cfcb5c7
Shaw Vrana <shaw@vranix.com>
2022-03-02 13:24:20 -0800

    Initial checkin
        * Include pdf as flake output
        * Makefile that runs the build command
        * Fill in resume.tex :P

---
+++ flake.nix
@@ -0,0 +1,19 @@
+{
+  description = "svrana resume flake";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShell = pkgs.mkShell {
+          nativeBuildInputs = [ pkgs.bashInteractive ];
+          buildInputs = [
+            pkgs.texlive.combined.scheme-basic
+          ];
+        };
+      });
+}
